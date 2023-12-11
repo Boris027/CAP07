9ca1ee02762fdbd51709e419205342dc3ff98041
Boris027 <titankiller027@gmail.com>
2023-12-11 20:22:29 +0100

    correccion ejercicio7

--- src/ejercicio7.java
+++ src/ejercicio7.java
@@ -21,15 +21,36 @@ public class ejercicio7 {
     }
 
     for(int i=0;i<=99;i++){
-      System.out.println("["+(i+1)+"]"+lista[i] +" ");
-    }
+      System.out.print(+lista[i] +" ");
 
+      if(i==50){
+        System.out.println("");
+      }
+    }
+    System.out.println("");
     System.out.println("Introduce el numero de los valores que quieres intercambiar");
     System.out.print("Primer numero: ");
     int num1=intercamio();
     System.out.print("Segundo numero: ");
     int num2=intercamio();
     
+
+    for(int i=0;i<=99;i++){
+      if(lista[i]==num1+1){
+        System.out.println(lista[i]);
+        num1=i;
+        break;
+      }
+    }
+
+    for(int i=0;i<=99;i++){
+      if(lista[i]==num2+1){
+        num2=i;
+        break;
+      }
+    }
+
+
     int repositorio1=lista[num1];
     int repositorio2=lista[num2];
     lista[num1]=repositorio2;
@@ -37,7 +58,18 @@ public class ejercicio7 {
     
 
     for(int i=0;i<=99;i++){
-      System.out.println("["+(i+1)+"]"+lista[i] +" ");
+      
+      if(i==num1){
+        System.out.print("*"+lista[i] +"* ");
+      }else if(i==num2){
+        System.out.print("*"+lista[i] +"* ");
+      }else{
+        System.out.print(""+lista[i] +" ");
+      }
+
+      if(i==50){
+        System.out.println("");
+      }
     }
 
 
